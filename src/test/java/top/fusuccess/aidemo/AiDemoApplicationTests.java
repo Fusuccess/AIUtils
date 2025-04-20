3871dd0b45f7b914d26175e32fe55b8f20093bf4
Fusuccess <fu2000520@tom.com>
2025-04-20 22:45:27 +0800

    first commit

---
+++ src/test/java/top/fusuccess/aidemo/AiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fusuccess.aidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

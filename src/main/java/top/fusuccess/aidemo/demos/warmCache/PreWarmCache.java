93221a17fede085f406144880377afbb58ec8187
Fusuccess <fu2000520@tom.com>
2025-04-24 18:14:29 +0800

    perf: ⚡ 项目启动缓存预热

---
+++ src/main/java/top/fusuccess/aidemo/demos/warmCache/PreWarmCache.java
@@ -0,0 +1,21 @@
+package top.fusuccess.aidemo.demos.warmCache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class PreWarmCache {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @PostConstruct
+    public void preWarmCache() {
+        // 从数据库或其他地方预加载数据并缓存
+        redisTemplate.opsForValue().set("你好", "你好，我是AIUtils工具搭建的AI智能聊天机器人，请问有什么可以帮助到您的呢？", 3600, TimeUnit.SECONDS);
+    }
+}
